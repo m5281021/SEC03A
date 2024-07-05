f3d37ace8a5881a66ca4f69c40fc6ff38878407e
s1290216 <s1290216@u-aizu.ac.jp>
2024-07-05 15:27:14 +0900

    adding some views

--- FirstPrj/FirstApp/views.py
+++ FirstPrj/FirstApp/views.py
@@ -22,8 +22,10 @@ def register_new_user(request):
 def register_new_movie_seats(request):
     try:
         with transaction.atomic():
+            u = User.objects.filter(name = user_name).get()
+            st = Showtime.objects.filter(number = showtime_number).get()
             seat = Seat.objects.select_for_update().all()
-            seat = Seat(showtime = showtime_number, reserved = True, user = user_name)
+            seat = Seat(showtime = st, reserved = True, user = u)
             seat.save()
     except register_new_movie_seats:
         return HttpResponse("Something went wrong.")
@@ -32,8 +34,8 @@ def register_new_movie_seats(request):
 def show_user_movies(request):
     try:
         with transaction.atomic():
-            movie = Movie.objects.filter(user__name = user_name)
-            print(movie)
+            s = Seat.objects.filter(user__name = user_name)
+            print(s)
     except show_user_movies:
         return HttpResponse("Something went wrong.")
     return HttpResponse("You can watch your movies!")
@@ -41,7 +43,7 @@ def show_user_movies(request):
 def show_matching_movies_date(request):
     try:
         with transaction.atomic():
-            show_movie = Showtime.objects.filter(times = date)
+            show_movie = Showtime.objects.filter(time = date)
             print(show_movie)
     except show_matching_movies_date:
         return HttpResponse("Something went wrong.")
